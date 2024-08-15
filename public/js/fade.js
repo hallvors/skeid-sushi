4ad02551113f7d0255cf851a5ed32d6c13c244d6
Hallvord R. M. Steen <hallvord@minus.no>
2024-08-15 20:15:52 +0200

    Less automatic switching if user is interacting with page

--- public/js/fade.js
+++ public/js/fade.js
@@ -1,15 +1,34 @@
-document.addEventListener("DOMContentLoaded", function (evt) {
-  var slideParent = document.getElementsByClassName("slides")[0];
-  var slides = slideParent.getElementsByTagName('div');
-  var current =
-    location.hash && location.hash.indexOf("#slide-") === 0
-      ? parseInt(location.hash.split("-")[1])
-      : 0;
-  setInterval(function () {
-    current++;
-    if (current >= slides.length) {
-        current = 0;
+(function () {
+  let currentSlideIndex;
+  let interval;
+
+  function nextSlide() {
+    var slideParent = document.getElementsByClassName("slides")[0];
+    var slides = slideParent.getElementsByTagName("div");
+    currentSlideIndex++;
+    if (currentSlideIndex >= slides.length) {
+      currentSlideIndex = 0;
     }
-    location.hash = '#slide-' + current;
-  }, 9000);
-});
+    location.hash = "#slide-" + currentSlideIndex;
+    window.scrollTo(0, 0);
+  }
+
+  function resetInterval() {
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(nextSlide, 9000);
+  }
+
+  document.addEventListener("DOMContentLoaded", function (evt) {
+    currentSlideIndex =
+      location.hash && location.hash.indexOf("#slide-") === 0
+        ? parseInt(location.hash.split("-")[1])
+        : 0;
+    interval = setInterval(nextSlide, 9000);
+  });
+
+  document.addEventListener('click', resetInterval, true);
+  document.addEventListener('mousemove', resetInterval, true);
+  document.addEventListener('touchmove', resetInterval, true);
+})();
